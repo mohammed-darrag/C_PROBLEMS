ae9cd63cbcd76ba0e5e73f3f66cd077e6b6a8b74
mohammed-darrag <mohammeddarrag13@gmail.com>
2024-05-29 19:14:29 +0200

    implesing casting

---
+++ basics_C/implesing casting.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  char c1=-1;
+  short s1=c1;
+
+  printf("%d %d",c1,s1);  -1 ,-1 
+
+
+   return 0;
+}
+
+///////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  unsigned char c1=-1;
+  short s1=c1;
+
+  printf("%d %d",c1,s1);  255 ,255 
+
+
+   return 0;
+}
